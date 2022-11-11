3792f217b02f3b5fb53bc553cfc521657c1afabe
Nayan1002 <nayansorathiya1002@gmail.com>
2022-11-11 13:46:11 -0500

    new directory changes

---
+++ Project/src/main/java/com/controller/dao/Dao.java
@@ -0,0 +1,88 @@
+package com.controller.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import com.project.model.Coin;
+
+
+public class Dao {
+	private String username="root";
+	private String password="Nz@123456789";
+	private String jdbcUrl ="jdbc:mysql://localhost:3306/cryptocurrencies?userSSL=false";
+	
+	private static final String SELECT_COIN_BY_ID = "select * from users where id=?;";
+	private static final String INSERT_COIN = "INSERT INTO coins"+" (name,price,startDate	) VALUES "+" (?,?,?);";
+	private static final String UPDATE_COIN="UPDATE user SET name=?,email=?,country=? where id=?;";
+	private static final String DELETE_COIN="DELETE from coins where id=?;";
+	private static final String SELECT_ALL_COINS="SELECT * from coins";
+	
+	protected Connection getConnection()
+	{
+		Connection conn = null;
+		try {
+		    conn =
+		       DriverManager.getConnection(jdbcUrl,username,password);
+		    	System.out.print("Successful connected");
+
+
+		} catch (SQLException ex) {
+		    // handle any errors
+		    System.out.println("SQLException: " + ex.getMessage());
+		    System.out.println("SQLState: " + ex.getSQLState());
+		    System.out.println("VendorError: " + ex.getErrorCode());
+		} catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	public List<Coin> selectAllUsers() {
+
+		// using try-with-resources to avoid closing resources (boiler plate code)
+		List<Coin> coins = new ArrayList<>();
+		// Step 1: Establishing a Connection
+		try (Connection connection = getConnection();
+
+				// Step 2:Create a statement using connection object
+			PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_COINS);) {
+			System.out.println(preparedStatement);
+			// Step 3: Execute the query or update query
+			ResultSet rs = preparedStatement.executeQuery();
+
+			// Step 4: Process the ResultSet object.
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				String startDate = rs.getString("startDate");
+				double price = rs.getDouble("price");
+				double volume24 = rs.getDouble("volume24");
+				coins.add(new Coin(id, name,price,startDate,volume24));
+			}
+		} catch (SQLException e) {
+			printSQLException(e);
+		}
+		return coins;
+	}
+	
+	private void printSQLException(SQLException ex) {
+		for (Throwable e : ex) {
+			if (e instanceof SQLException) {
+				e.printStackTrace(System.err);
+				System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+				System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+				System.err.println("Message: " + e.getMessage());
+				Throwable t = ex.getCause();
+				while (t != null) {
+					System.out.println("Cause: " + t);
+					t = t.getCause();
+				}
+			}
+		}
+	}
+}
---
+++ Project/src/main/java/com/controller/model/Coin.java
@@ -0,0 +1,56 @@
+package com.project.model;
+
+public class Coin {
+	private int id;
+	private String name;
+	private double price;
+	private String startDate;
+	private double volume24;
+	
+	
+	public Coin(int id,String name,double price,String startDate,double volume)
+	{
+		this.id=id;
+		this.name=name;
+		this.price=price;
+		this.startDate=startDate;
+		this.volume24=volume24;
+	}
+	
+	public Coin(String name,double price,String startDate,double volume)
+	{
+		this.id=id;
+		this.name=name;
+		this.price=price;
+		this.startDate=startDate;
+		this.volume24=volume24;
+	}
+	
+	public int getId()
+	{
+		return id;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public double getPrice()
+	{
+		return price;
+	}
+	
+	public double getVolume24()
+	{
+		return volume24;
+	}
+	
+	public String getStartDate()
+	{
+		return startDate;
+	}
+	
+	
+
+}
